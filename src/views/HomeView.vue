5d651245ba45c7dc3a54a5efa733b951bea8bea6
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-05-21 18:30:54 +0300

    card added...not working

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,16 @@
 <template>
   <search-bar-vue />
 </template>
-
 <script lang="ts">
 import Vue from 'vue'
 import SearchBarVue from '@/components/SearchBar.vue'
-
+import CardVue from '@/components/Card.vue'
 export default Vue.extend({
   name: 'Home',
-
   components: {
-    SearchBarVue
+    SearchBarVue,
+    /* eslint-disable */
+    CardVue
   }
 })
 </script>
